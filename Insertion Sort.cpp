913503272610eadd368e2bd2a2df0ec90a43d272
Rizwan Ahmed <35874979+Riz1ahmed@users.noreply.github.com>
2019-12-13 11:39:43 +0600

    Create Insertion Sort.cpp

---
+++ Insertion Sort.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i-1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j = j-1;
+        }
+        arr[j+1] = key;
+    }
+    for (i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
