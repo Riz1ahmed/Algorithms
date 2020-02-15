f88acd151e367f3ff16baecf440776e172b62b1e
Rizwan Ahmed <35874979+Riz1ahmed@users.noreply.github.com>
2020-02-15 23:29:11 +0600

    Update Warshall Algorithm.cpp

--- Warshall Algorithm.cpp
+++ Warshall Algorithm.cpp
@@ -1,23 +1,20 @@
-#include <cstdio>
-#define inf 1e7
-#define boom(a) memset(a,0,sizeof(a))
+/***********  Warshall Algorithm  *************
+Given a wighted graph with N node & E e edges.
+Find all possible shortest path.    **********/
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-	//freopen("Warshal.txt","r",stdin);
+int main(){
 	int n,e,u,v,w,i,j,k,ans=0;
-	scanf("%d %d",&n,&e);
+	cin>>n>>e;
 	int mat[n+2][n+2];
-	for (i=0; i<=n; i++)
-		for (j=i; j<=n; j++)
-			mat[i][j]=mat[j][i]=inf;
-	while (e--){
-		scanf("%d %d %d",&u,&v,&w);
-		mat[v][u]=mat[u][v]=w;
-	}
-	printf("\nThe matrix is\n");
+	for (i=0; i<=n; i++) for (j=i; j<=n; j++)
+        mat[i][j]=mat[j][i]=1e7;
+	while (e--) cin>>u>>v>>w, mat[v][u]=mat[u][v]=w;
+
+	puts("\nThe matrix is");
 	for (i=1; i<=n; i++){
 		for (j=1; j<=n; j++)
-			printf(mat[i][j]==inf ? " -1 ":"%3d ",mat[i][j]);
+			printf(mat[i][j]==1e7 ? " -1 ":"%3d ",mat[i][j]);
 		puts("");
 	}
 	for (k=1; k<=n; k++) ///Warshall ALGO
@@ -25,10 +22,10 @@ int main() {
 			for (j=1; j<=n; j++)
 				if ( i!=j && mat[i][k]+mat[k][j] < mat[i][j])
 					mat[i][j] = mat[j][i] = mat[i][k] + mat[k][j];
-	printf("\nNow the matrix is\n");
+	puts("\nNow the matrix is");
 	for (i=1; i<=n; i++){
 		for (j=1; j<=n; j++)
-			printf(mat[i][j]==inf ? " -1 ":"%3d ",mat[i][j]);
+			printf(mat[i][j]==1e7 ? " -1 ":"%3d ",mat[i][j]);
 		puts("");
 	}
 	return 0;
