df32ec1b81d2118f4773f69ce90e7ce8d7471406
Rizwan Ahmed <35874979+Riz1ahmed@users.noreply.github.com>
2020-02-15 23:19:39 +0600

    Update Kruskal Algorithm.cpp

--- Kruskal Algorithm.cpp
+++ Kruskal Algorithm.cpp
@@ -1,38 +1,34 @@
-#include <cstdio>
-#include <map>
-#include <algorithm>
-#define SZ 80000
+/***** Minimum Spanning Tree (Kruskal) Algorithm ******
+Given a wighted graph with N node & E e edges.
+Find Minimum Spanning Tree. Traverse all Node with minCost.*/
+#include <bits/stdc++.h>
 using namespace std;
 struct eg{ int u,v; };
-int parent[SZ];
-int findp(int x) {
-    if (parent[x]==x) return x;
-    return parent[x]=findp(parent[x]);
+int par[80000];
+int findP(int x) {
+    return par[x] == x ? x : (par[x]=findP(par[x]));
 }
-int main() {
-	freopen("Prims.txt","r",stdin);
+int main(){
     int n,e,i,j,k,f;
     map<int,eg>mp;
-	scanf("%d %d",&n,&e);
+	cin>>n>>e;
 	int ans=0,u,v,w[n+2];
-	for (i=0; i<=n; i++) parent[i]=i;
-	for (i=0; i<e; i++){
-		scanf("%d %d %d",&u,&v,&w[i]);
-		eg x; x.u=u,x.v=v;
-		mp[w[i]]=x;
-	}
+	for (i=0; i<=n; i++) par[i]=i;
+	for (i=0; i<e; i++)
+		cin>>u>>v>>w[i],
+		mp[w[i]]={u,v};
+
 	sort(w,w+e);
 	for (i=0; i<e; i++){
-		printf("%d %d %d ", w[i], mp[w[i]].u, mp[w[i]].v);
-		u=mp[w[i]].u,v=mp[w[i]].v;
-		u=findp(u),v=findp(v);
-		if (u!=v){
-			printf("= Taken");
-			parent[u]=v;
-			ans+=w[i];
-		} else printf("= Not taken");
-	puts("");
+		cout<<w[i]<<" "<<mp[w[i]].u<<" "<<mp[w[i]].v<<" ";
+		u=mp[w[i]].u, v=mp[w[i]].v;
+		u=findP(u), v=findP(v);
+		if (u==v) puts("= Already Connected");
+		else{
+			puts("= Connected OK");
+			par[u]=v, ans+=w[i];
+		}
 	}
-	printf("\nMinimum Spening Tree with Kruskal is %d\n",ans);
+	printf("\nMinimum Spanning Tree with Kruskal is %d\n",ans);
     return 0;
 }
