e27557319f916b81cda92393d9189b633e5ea18b
Rizwan Ahmed <35874979+Riz1ahmed@users.noreply.github.com>
2020-03-30 06:34:09 +0600

    Update Custom input.cpp

--- Custom input.cpp
+++ Custom input.cpp
@@ -1,15 +1,15 @@
 ///Fast Custom Sentence and Integer Input.
 #include<stdio.h>
 #include<string.h>
-void Sentance(char s[]){
+void Sentence(char s[]){
 	char c; int i=0;
 	while((c=getchar())!='\n')
 		s[i++]=c;
 	s[i]='\0';
 }
 
-int Read(ll &n){
-    int sign=1; c=getchar(),n=0;
+int Read(int &n){
+    int sign=1; char c=getchar(); n=0;
     for(; c<48 || 87<c; c=getchar())
         if(c=='-') sign=-1;
     for(; 47<c && c<58; c=getchar())
@@ -20,6 +20,7 @@ int Read(ll &n){
 int main(){
 	int n;
 	char s[1000];
-	Sentence(s), Read(n);
+	Sentence(s);
+	Read(n);
 	printf("%s %d\n",s,n);
 }
